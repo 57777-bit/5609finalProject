57006e5df49cafac6043b82810cb3ab1124a3146
Qi Wu <wu001656@umn.edu>
2026-04-12 13:38:06 -0500

    fix github pages base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,27 @@
+// import adapter from '@sveltejs/adapter-static';
+
+// export default {
+//   kit: {
+//     adapter: adapter({
+//       pages: 'build',
+//       assets: 'build',
+//       fallback: '404.html',
+//       precompress: false,
+//       strict: true
+//     })
+//   }
+// };
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+const dev = process.argv.includes('dev');
+
+const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/5609finalProject'
+    }
   }
 };
+
+export default config;
